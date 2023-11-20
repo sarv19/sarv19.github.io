c0d67f318436112f15f88e1bfdcc3a63cdafedc4
Sarah <vongocquynhnhu@gmail.com>
2023-11-19 21:19:15 -0500

    update about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,8 +13,6 @@ const About = () => {
                 Aenean commodo ligula eget dolor. Aenean massa.
                 Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
                 Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
-                Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec,
-                vulputate eget, arcu.
             </p>
         </Element>
     )
